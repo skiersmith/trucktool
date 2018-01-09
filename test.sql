5f4207d30f010406c30c28b1b4bfdf7f15c1000c
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2018-01-09 14:57:45 -0700

    sql examples

--- test.sql
+++ test.sql
@@ -6,3 +6,59 @@
 --     PRIMARY KEY (id),
 --     UNIQUE KEY email (email)
 -- );
+
+-- CREATE TABLE vaults (
+--     id int NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(20) NOT NULL,
+--     description VARCHAR(255) NOT NULL,
+--     userId int,
+--     INDEX userId (userId),
+--     FOREIGN KEY (userId)
+--         REFERENCES users(id)
+--         ON DELETE CASCADE,  
+--     PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE keeps (
+--     id int NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(20) NOT NULL,
+--     description VARCHAR(255) NOT NULL,
+--     userId int,
+--     INDEX userId (userId),
+--     FOREIGN KEY (userId)
+--         REFERENCES users(id)
+--         ON DELETE CASCADE,  
+--     PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE vaultkeeps (
+--     id int NOT NULL AUTO_INCREMENT,
+--     vaultId int NOT NULL,
+--     keepId int NOT NULL,
+--     userId int NOT NULL,
+
+--     PRIMARY KEY (id),
+--     INDEX (vaultId, keepId),
+--     INDEX (userId),
+
+--     FOREIGN KEY (userId)
+--         REFERENCES users(id)
+--         ON DELETE CASCADE,
+
+--     FOREIGN KEY (vaultId)
+--         REFERENCES vaults(id)
+--         ON DELETE CASCADE,
+
+--     FOREIGN KEY (keepId)
+--         REFERENCES keeps(id)
+--         ON DELETE CASCADE
+-- )
+
+-- SELECT * FROM vaultkeeps vk
+-- INNER JOIN keeps k ON k.id = vk.keepId 
+-- WHERE (vaultId = 2)
+
+-- DELETE FROM keeps WHERE id = 1;
+
+-- DELETE from vaults WHERE id = 2;
+-- DELETE FROM users WHERE id = 1;
