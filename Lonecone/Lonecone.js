4917ec44cad6a7f6b09a391b44bd683ed029b6f7
Hunter Smith <skiersmith21@gmail.com>
2018-02-23 17:41:54 -0700

    removed stuff

--- Lonecone/Lonecone.js
+++
@@ -1,8 +0,0 @@
-
-
-function parse(){if(typeof require !== 'undefined') XLSX = require('xlsx');
-var workbook = XLSX.readFile('LossOnReturns.xlsx');
-return workbook
-debugger
-}
-console.log(parse())
